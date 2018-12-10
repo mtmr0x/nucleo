39a9b5eefaeb9cee61409ac302088b384ce35f40
Rogerio Moura <roger0.rm@gmail.com>
2018-12-10 10:43:19 -0200

    Fix word 'try' into specs

--- src/nucleoTypes/NucleoNumberAssertion.spec.ts
+++ src/nucleoTypes/NucleoNumberAssertion.spec.ts
@@ -26,7 +26,7 @@ describe('NucleoNumberAssertion', () => {
   const store = createStore(contracts);
   const { dispatch, update, cloneState } = store;
 
-  it('should ty to violate contract with wrong type dispatching to contract', () => {
+  it('should try to violate contract with wrong type dispatching to contract', () => {
     const d = dispatch('user')({
       name: 'John',
       age: 110
